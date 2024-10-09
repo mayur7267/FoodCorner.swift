9109b5cd2938ba265e14609d9f8d72f56bf97fdc
mayur7267 <mayurlate.7267@gmail.com>
2024-10-10 03:00:10 +0530

    Initial Commit

---
+++ FoodCorner.swift/FoodCorner_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodCorner_swiftApp.swift
+//  FoodCorner.swift
+//
+//  Created by Mayur on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct FoodCorner_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FoodCorner.swiftTests/FoodCorner_swiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  FoodCorner_swiftTests.swift
+//  FoodCorner.swiftTests
+//
+//  Created by Mayur on 10/10/24.
+//
+
+import Testing
+@testable import FoodCorner_swift
+
+struct FoodCorner_swiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
